ee9da3e7bb9156f60cb639314f063ab8edce7e90
akshayblevel <akshayblevel@gmail.com>
2021-07-22 19:30:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-zsthca
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-zsthca)
---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Greet() {
+  return <div>Hello Akshay!</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Greet />
+      <Greet />
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
